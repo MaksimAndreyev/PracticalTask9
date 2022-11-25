bb9fdf9e5d2957036e5eb0002d86c440bb191b4e
maximandreev <maxands@yandex.ru>
2022-11-25 15:41:11 +0300

    check_field modified

--- TicTacToe.py
+++ TicTacToe.py
@@ -24,27 +24,20 @@ class TicTacToeBoard:
         return output
 
     def check_field(self):
-        if self.board[0][0]+self.board[1][1]+self.board[2][2] == 3 or \
-            self.board[0][2]+self.board[1][1]+self.board[2][0] == 3 or \
-            self.board[0][0] + self.board[0][1] + self.board[0][2] == 3 or \
-            self.board[1][0] + self.board[1][1] + self.board[1][2] == 3 or \
-            self.board[2][0] + self.board[2][1] + self.board[2][2] == 3 or \
-            self.board[0][0] + self.board[1][0] + self.board[2][0] == 3 or \
-            self.board[0][1] + self.board[1][1] + self.board[2][1] == 3 or \
-                self.board[0][2] + self.board[1][2] + self.board[2][2] == 3:
-            self.result = 'X'
-        elif self.board[0][0]+self.board[1][1]+self.board[2][2] == -3 or \
-            self.board[0][2]+self.board[1][1]+self.board[2][0] == -3 or \
-            self.board[0][0] + self.board[0][1] + self.board[0][2] == -3 or \
-            self.board[1][0] + self.board[1][1] + self.board[1][2] == -3 or \
-            self.board[2][0] + self.board[2][1] + self.board[2][2] == -3 or \
-            self.board[0][0] + self.board[1][0] + self.board[2][0] == -3 or \
-            self.board[0][1] + self.board[1][1] + self.board[2][1] == -3 or \
-                self.board[0][2] + self.board[1][2] + self.board[2][2] == -3:
-            self.result = '0'
+        if abs(self.board[0][0]+self.board[1][1]+self.board[2][2]) == 3 or \
+            abs(self.board[0][2]+self.board[1][1]+self.board[2][0]) == 3 or \
+            abs(self.board[0][0] + self.board[0][1] + self.board[0][2]) == 3 or \
+            abs(self.board[1][0] + self.board[1][1] + self.board[1][2]) == 3 or \
+            abs(self.board[2][0] + self.board[2][1] + self.board[2][2]) == 3 or \
+            abs(self.board[0][0] + self.board[1][0] + self.board[2][0]) == 3 or \
+            abs(self.board[0][1] + self.board[1][1] + self.board[2][1]) == 3 or \
+                abs(self.board[0][2] + self.board[1][2] + self.board[2][2]) == 3:
+            if self.turn == 1:
+                self.result = 'X'
+            else:
+                self.result = '0'
         elif sum(self.board[0])+sum(self.board[1])+sum(self.board[2]) == 1:
             self.result = 'D'
-        return self.result
 
     def make_move(self, row, col):
         if self.result != None:
@@ -52,8 +45,8 @@ class TicTacToeBoard:
         if self.board[row-1][col-1] != 10:
             return f'Клетка {row}, {col} уже занята'
         self.board[row-1][col-1] = self.turn
-        self.turn = -self.turn
         self.check_field()
+        self.turn = -self.turn
         if self.result == 'D':
             return 'Ничья'
         if self.result == None:
